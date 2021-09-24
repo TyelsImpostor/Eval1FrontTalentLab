c7cf22bf073875aa888523a233ee3bbbe9235096
su nombre <su_email@gmail.com>
2021-09-24 14:23:07 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './compartida/navbar/navbar.component';
+import { FooterComponent } from './compartida/footer/footer.component';
+import { LoginComponent } from './general/login/login.component';
+import { RegistroComponent } from './general/registro/registro.component';
+import { ProductosComponent } from './general/productos/productos.component';
+import { BuscarPorCategoriaComponent } from './compartida/buscar-por-categoria/buscar-por-categoria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    RegistroComponent,
+    ProductosComponent,
+    BuscarPorCategoriaComponent
   ],
   imports: [
     BrowserModule
